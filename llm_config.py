24f845294d5421668786a46e350a1f1cf06c0e58
Hariprasath2706 <hariprasaths270606@gmail.com>
2026-03-06 15:43:54 +0530

    Create llm_config.py

---
+++ llm_config.py
@@ -0,0 +1,7 @@
+def generate_analysis():
+
+    print("\nAI Analysis:")
+    print("• India and England appear strongest in recent matches.")
+    print("• Finn Allen and Sahibzada Farhan are top batting performers.")
+    print("• Adil Rashid and Varun Chakaravarthy lead bowling charts.")
+    print("• Spin bowlers are dominating this tournament.")
