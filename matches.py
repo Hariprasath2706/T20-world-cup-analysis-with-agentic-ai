cf9f1cb6d5055e0798f096546a942c1b964af4c5
Hariprasath2706 <hariprasaths270606@gmail.com>
2026-03-06 15:40:14 +0530

    Create matches.py

---
+++ matches.py
@@ -0,0 +1,12 @@
+[
+  {"match": "IND vs ENG", "result": "India won by 7 runs"},
+  {"match": "NZ vs SA", "result": "New Zealand won by 9 wickets"},
+  {"match": "IND vs WI", "result": "India won by 5 wickets"},
+  {"match": "PAK vs SL", "result": "Pakistan won by 5 runs"},
+  {"match": "ENG vs WI", "result": "England won by 3 wickets"},
+  {"match": "NZ vs PAK", "result": "New Zealand won by 6 runs"},
+  {"match": "SA vs IND", "result": "South Africa won by 4 wickets"},
+  {"match": "ENG vs SL", "result": "England won by 8 runs"},
+  {"match": "WI vs PAK", "result": "West Indies won by 7 wickets"},
+  {"match": "IND vs NZ", "result": "India won by 12 runs"}
+]
