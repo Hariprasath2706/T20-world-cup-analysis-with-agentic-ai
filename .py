c9fbc09315ba9c31fcc63e79e5d4d2c8d3109256
Hariprasath2706 <hariprasaths270606@gmail.com>
2026-03-06 15:39:16 +0530

    Create .py

---
+++ .py
@@ -0,0 +1,26 @@
+{
+  "batting": [
+    {"player": "Sahibzada Farhan", "team": "PAK", "runs": 383},
+    {"player": "Finn Allen", "team": "NZ", "runs": 289},
+    {"player": "Jacob Bethell", "team": "ENG", "runs": 280},
+    {"player": "Aiden Markram", "team": "SA", "runs": 286},
+    {"player": "Ishan Kishan", "team": "IND", "runs": 263},
+    {"player": "Suryakumar Yadav", "team": "IND", "runs": 242},
+    {"player": "Shimron Hetmyer", "team": "WI", "runs": 248},
+    {"player": "Harry Brook", "team": "ENG", "runs": 235},
+    {"player": "Tim Seifert", "team": "NZ", "runs": 274},
+    {"player": "Babar Azam", "team": "PAK", "runs": 210}
+  ],
+  "bowling": [
+    {"player": "Adil Rashid", "team": "ENG", "wickets": 13},
+    {"player": "Varun Chakaravarthy", "team": "IND", "wickets": 13},
+    {"player": "Blessing Muzarabani", "team": "ZIM", "wickets": 13},
+    {"player": "Lungi Ngidi", "team": "SA", "wickets": 12},
+    {"player": "Rachin Ravindra", "team": "NZ", "wickets": 11},
+    {"player": "Maheesh Theekshana", "team": "SL", "wickets": 11},
+    {"player": "Corbin Bosch", "team": "SA", "wickets": 11},
+    {"player": "Jofra Archer", "team": "ENG", "wickets": 11},
+    {"player": "Marco Jansen", "team": "SA", "wickets": 11},
+    {"player": "Shaheen Afridi", "team": "PAK", "wickets": 10}
+  ]
+}
